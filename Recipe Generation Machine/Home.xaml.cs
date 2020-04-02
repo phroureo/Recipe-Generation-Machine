21cb1e2a1f8b432854d8bd5814c1b43cbeb5d2ee
Dallin Stone <castle91@gmail.com>
2020-04-02 10:10:23 -0500

    first upload of rgm

---
+++ Recipe Generation Machine/Home.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Recipe_Generation_Machine
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainFrame_Initialized(object sender, EventArgs e)
+        {
+            WelcomePage wp = new WelcomePage();
+            mainFrame.Navigate(wp);
+        }
+    }
+}
